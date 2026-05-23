d6a47a4a0c6c6d09c06437279d77946a5ad235e0
jeeva <jeevaprakashdr@gmail.com>
2026-05-24 00:50:13 +0200

    codecrafters submit [skip ci]

--- src/core.rs
+++ src/core.rs
@@ -1,30 +1,109 @@
-#[derive(Default)]
-pub(crate) struct Message {
-    inner: [u8; 12],
+pub(crate) struct Message<'a> {
+    inner: [u8; 10],
     pub(crate) header: Header,
+    pub(crate) question: Question<'a>,
 }
 
-impl Message {
+impl<'a> Message<'a> {
     pub(crate) fn new() -> Self {
-        Self::default()
+        Self {
+            inner: [0u8; 10],
+            header: Header::default(),
+            question: Question::default(),
+        }
     }
 
-    pub(crate) fn as_slice(&mut self) -> &[u8] {
-        self.inner[..4].copy_from_slice(&self.header.inner);
-        self.inner.as_slice()
+    pub(crate) fn to_vec(&mut self) -> Vec<u8> {
+        let mut data = Vec::new();
+        data.extend_from_slice(&self.header.inner);
+        data.extend_from_slice(&self.question.as_bytes());
+        data
+    }
+
+    pub(crate) fn set_question(&mut self, name: &'a str, qtype: QType, qclass: QClass) {
+        self.question.name = name;
+        self.question.qtype = qtype;
+        self.question.qclass = qclass;
+        self.header.set_question_no(self.question.as_bytes().len() as u16);
+    }
+}
+
+#[derive(Default)]
+pub(crate) struct Question<'a> {
+    name: &'a str,
+    qtype: QType,
+    qclass: QClass,
+}
+
+impl<'a> Question<'a> {
+    pub(crate) fn new(name: &'a str, qtype: QType, qclass: QClass) -> Self {
+        Self {
+            name,
+            qtype,
+            qclass,
+        }
+    }
+
+    fn as_bytes(&self) -> Vec<u8> {
+        let mut inner = Vec::<u8>::new();
+        inner.extend_from_slice(self.encode_name().as_slice());
+        inner.extend_from_slice(b"0".as_slice());
+        inner.extend_from_slice(&self.qtype.to_byte().to_be_bytes());
+        inner.extend_from_slice(&self.qclass.to_byte().to_be_bytes());
+        inner
+    }
+
+    fn encode_name(&self) -> Vec<u8> {
+        let domain = self.name.split(".").collect::<Vec<_>>();
+        let base = domain.first().unwrap();
+        let tld = domain.last().unwrap();
+        let encoded_string = format!("{}{base}{}{tld}", base.len(), tld.len());
+        encoded_string.as_bytes().to_vec()
+    }
+}
+
+pub(crate) enum QClass {
+    IN,
+}
+
+impl QClass {
+    pub(crate) fn to_byte(&self) -> u8 {
+        match self {
+            QClass::IN => 1,
+        }
+    }
+}
+
+impl Default for QClass {
+    fn default() -> Self {
+        QClass::IN
+    }
+}
+
+pub(crate) enum QType {
+    A,
+}
+
+impl QType {
+    pub(crate) fn to_byte(&self) -> u8 {
+        match self {
+            QType::A => 1,
+        }
+    }
+}
+
+impl Default for QType {
+    fn default() -> Self {
+        QType::A
     }
 }
 
 #[derive(Default)]
 pub(crate) struct Header {
-    inner: [u8; 4],
+    inner: [u8; 12],
 }
 
 impl Header {
-    pub(crate) fn new() -> Self {
-        Self::default()
-    }
-
     pub(crate) fn set_id(&mut self, id: u16) {
         self.inner[..2].copy_from_slice(&id.to_be_bytes())
     }
@@ -35,5 +114,9 @@ impl Header {
         } else {
             self.inner[2] &= !(1 << 7)
         }
+    }
+
+    fn set_question_no(&mut self, count: u16) {
+        self.inner[4..6].copy_from_slice(&count.to_be_bytes());
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #[allow(unused_imports)]
 use std::net::UdpSocket;
 
-use crate::core::Message;
+use crate::core::{Message, QClass, QType};
 
 mod core;
 
@@ -16,11 +16,12 @@ fn main() {
                 let mut message = Message::new();
                 message.header.set_id(u16::try_from(1234).unwrap());
                 message.header.set_qr(true);
+                message.set_question("codecrafter.com", QType::A, QClass::IN);
 
-                println!("{:?}", &message.as_slice());
+                println!("{:?}", &message.to_vec());
 
                 udp_socket
-                    .send_to(&message.as_slice(), source)
+                    .send_to(&message.to_vec(), source)
                     .inspect(|f| println!("passed {}", f))
                     .expect("Failed to send response");
             }
