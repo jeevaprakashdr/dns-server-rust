ff74b717027b2ea8c95c55e4f563e2ebe988dca3
jeeva <jeevaprakashdr@gmail.com>
2026-05-25 20:59:32 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -17,12 +17,12 @@ fn main() {
                 let id = &buf[..2].to_vec();
                 let mut message = Message::new();
                 message.header.set_id(id);
-                message.header.set_opcode(&buf[2]);
                 message.header.set_qr();
                 message.header.set_rcode();
+                message.header.set_opcode(&buf[2]);
                 message.set_question("codecrafters.io".to_string(), QType::A, QClass::IN);
                 message.set_answer("codecrafters.io".to_string(), QType::A, QClass::IN);
-                
+
                 let message = message.to_vec();
                 println!("{:?}", message);
 
